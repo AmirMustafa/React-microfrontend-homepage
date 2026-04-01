a3ab52a19885c8c7c864db6e4203409e26217aba
Amir Mustafa <amirengg15@gmail.com>
2026-04-01 17:48:36 +0530

    fix: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import HomePage from './Components/HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <HomePage />
-    </div>
+   <HomePage />
   );
 }
 
