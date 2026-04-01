bdb9dadfee9eccd337f36a9cc8d90b0ba9adc186
Amir Mustafa <amirengg15@gmail.com>
2026-04-01 17:12:26 +0530

    feat: homepage microfrontend mfe

---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+    return (
+        <>
+            <h2>Hello, Welcome to React Micro frontend</h2>
+            <p>Happy Learnings !!</p>
+            <p>To enroll on homepage click on button!</p>
+
+        </>
+    )
+}
+
+export default HomePage;
